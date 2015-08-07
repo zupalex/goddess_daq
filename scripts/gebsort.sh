2821ffadc57d071b7ed9de050bdf2160031d42e6
Karl Smith <ksmit218@utk.edu>
2015-08-07 18:23:58 -0500

    Corected typo.

--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -13,8 +13,8 @@ echo "GEBSort started sorting run $RUN at `date`"
 if [ ! -e log ]; then
 	mkdir log
 fi
-if [ ! -e rootfile ]; then
-	mkdir rootfile
+if [ ! -e rootfiles ]; then
+	mkdir rootfiles
 fi
 ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd -rootfile
 rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat > log/GEBSort_run$RUN.log
