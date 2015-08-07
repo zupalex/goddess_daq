f767569bce5357ba650ecdc1d71c93532393c1ec
Karl Smith <ksmit218@utk.edu>
2015-08-07 13:31:39 -0500

    Added folder for chat files.

--- scripts/gebmerge.sh
+++ scripts/gebmerge.sh
@@ -12,11 +12,27 @@ DIR=$1
 # data from DGS, new data format (firmware) 9/25/2012
 
 echo "GEBMerge started at `date`"
-	./hribfConvert `ls $DIR/run$RUN.ldf` run$RUN.geb
-	if [ $? != 0 ]; then 
-		exit 1
-	fi
-  ./GEBMerge GEBMerge.chat Merged/GEBMerged_run$RUN.gtd `ls $DIR/run_gsfma330_$RUN.*` `ls $DIR/run_$RUN.dgs*` `ls run$RUN.geb` > LOG_FILES/GEBMerge_run$RUN.log
+
+if [ ! -e gebfiles ]; then
+	mkdir gebfiles
+fi
+
+./hribfConvert `ls $DIR/run$RUN.ldf` gebfiles/run$RUN.geb
+
+if [ $? != 0 ]; then 
+	exit 1
+fi
+
+if [ ! -e log ]; then
+	mkdir log
+fi
+if [ ! -e merged ]; then
+	mkdir merged
+fi
+./GEBMerge chatfiles/GEBMerge.chat merged/GEBMerged_run$RUN.gtd \
+	`ls $DIR/run_gsfma330_$RUN.*` `ls $DIR/run_$RUN.dgs*` `ls gebfiles/run$RUN.geb` \
+	 > log/GEBMerge_run$RUN.log
+
 echo "GEBMerge DONE at `date`"
 
 exit
--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -10,7 +10,8 @@ RUN=$2
 DIR=$1
 
 echo "GEBSort started sorting run $RUN at `date`"
-./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd -rootfile ./ROOT_FILES/run$RUN.root RECREATE -chat GEBSort.chat > ./LOG_FILES/GEBSort_run$RUN.log
+./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd -rootfile
+./ROOT_FILES/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat > ./LOG_FILES/GEBSort_run$RUN.log
 echo "GEBSort DONE at `date`"
 
 #exit
