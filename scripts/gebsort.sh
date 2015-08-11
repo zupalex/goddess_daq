0eec413900a26d5e574b21da6e854cfc51309fbb
Karl Smith <ksmit218@utk.edu>
2015-08-11 15:52:05 -0500

    Changed gebsort log move to copy.

--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -19,7 +19,7 @@ fi
 time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat > log/GEBSort_current.log
 echo "GEBSort DONE at `date`"
 
-mv log/GEBSort_current.log log/GEBSort_run$RUN.log
+cp -f log/GEBSort_current.log log/GEBSort_run$RUN.log
 
 tail -n 5 log/GEBSort_run$RUN.log
 
