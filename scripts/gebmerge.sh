08c2e1aa1514b63cc7f47fb6285784733a76ed20
Karl Smith <ksmit218@utk.edu>
2015-08-11 17:36:50 -0500

    Added color to gebmerge.

--- scripts/gebmerge.sh
+++ scripts/gebmerge.sh
@@ -2,6 +2,12 @@
 # The data directory may or may not contain the final run folder name
 # The merge directory must exist. 
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 if [ $# -ne 3 ] 
   then
    echo "USAGE: gebmerge.sh <data_dir> <merge_dir> <run>"
@@ -14,7 +20,7 @@ DATA_DIR=$1
 
 #Check for data folder
 if [ ! -e $DATA_DIR ]; then
-	echo "Data directory $DATA_DIR does not exist!"
+	printf "${RED}ERROR:${RESET} Data directory $DATA_DIR does not exist!\n"
 	exit 2
 fi
 
@@ -25,7 +31,7 @@ fi
 
 #Check for merge folder
 if [ ! -e $MERGE_DIR ]; then
-	echo "Merged directory $MERGE_DIR does not exist!"
+	printf "${RED}ERROR:${RESET} Merged directory $MERGE_DIR does not exist!\n"
 	exit 3
 fi
 
@@ -38,11 +44,11 @@ fi
 
 #convert ldf to a format that can be merged.
 if [ -e "$DATA_DIR/run$RUN.ldf" ]; then
-	echo "Converting ldf to GEB format."
+	echo "${BLUE}Converting ldf to GEB format.${RESET}"
 	ORNL_FILES=.run$RUN.geb
 	./hribfConvert `ls $DATA_DIR/run$RUN.ldf` .run$RUN.geb
 else
-	echo "WARNING: No ORNL ldfs found!"
+	printf "${YELLOW}WARNING:${RESET} No ORNL ldfs found!\n"
 fi
 
 #Check if previous command was successfull
@@ -52,11 +58,11 @@ fi
 
 DFMA_FILES=`ls $DATA_DIR/run_gsfma330_$RUN.* 2>/dev/null`
 if [ $? != 0 ]; then
-	echo "WARNING: No DFMA files found!"
+	printf "${YELLOW}WARNING:${RESET} No DFMA files found!\n"
 fi
 DGS_FILES=`ls $DATA_DIR/run_$RUN.dgs* 2>/dev/null`
 if [ $? != 0 ]; then
-	echo "WARNING: No DGS files found!"
+	printf "${YELLOW}WARNING:${RESET} No DGS files found!\n"
 fi
 
 echo "GEBMerge started at `date`"
@@ -65,10 +71,14 @@ echo "GEBMerge started at `date`"
 	$DFMA_FILES $DGS_FILES $ORNL_FILES \
 	 > log/GEBMerge_run$RUN.log
 
+err=$?
 #Remove intermediate converted file
 rm -f .run$RUN.geb
-
-echo "GEBMerge DONE at `date`"
+if [ $err == 0 ]; then
+	printf "${GREEN}GEBMerge DONE at `date`${RESET}\n"
+else
+	printf "${RED}GEBMerge FAILED at `date`${RESET}\n"
+fi
 
 tail log/GEBMerge_run$RUN.log -n 3
 
