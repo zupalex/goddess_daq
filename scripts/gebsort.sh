0d78297a01f971c20bcc58d264d9e82f05fabe85
Karl Smith <ksmit218@utk.edu>
2015-08-11 14:23:21 -0500

    Gebsort log now uses current until complete.
    The log file produced is GEBSort_current.log which is moved after the
    sort is complete.

--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 2 ] 
   then
-   echo "USAGE: gebmerge.sh <merged_dir> <run>"
+   echo "USAGE: gebsort.sh <merged_dir> <run>"
   exit 1
 fi
 
@@ -16,8 +16,12 @@ fi
 if [ ! -e rootfiles ]; then
 	mkdir rootfiles
 fi
-time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat > log/GEBSort_run$RUN.log
+time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile
+rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat >
+log/GEBSort_current.log
 echo "GEBSort DONE at `date`"
+
+mv log/GEBSort_current.log log/GEBSort_run$RUN.log
 
 tail -n 5 log/GEBSort_run$RUN.log
 
