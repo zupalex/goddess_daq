75be8e3920f614cf59b6d7e8346726cea7413ce4
arat <aratkiewicz@gmail.com>
2015-08-13 00:56:46 -0500

    fixed a shadowed def i missed

--- goddess/source/SolidVector.cxx
+++ goddess/source/SolidVector.cxx
@@ -1,7 +1,7 @@
 #include "SolidVector.h"
 
-SolidVector::SolidVector(double x, double y, double z, double rotZ /*=0*/, double rot_Phi /*=0*/) :
-	TVector3(x,y,z), rotZ(rotZ), rotPhi(rot_Phi)
+SolidVector::SolidVector(double x, double y, double z, double rot_Z /*=0*/, double rot_Phi /*=0*/) :
+	TVector3(x,y,z), rotZ(rot_Z), rotPhi(rot_Phi)
 {
 
 }
