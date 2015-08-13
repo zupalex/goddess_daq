898c8ffe5b78f2e8a55e34fe01101581651a1874
arat <aratkiewicz@gmail.com>
2015-08-13 00:53:38 -0500

    fixed shadowed def of rotPhi

--- goddess/source/SolidVector.cxx
+++ goddess/source/SolidVector.cxx
@@ -1,7 +1,7 @@
 #include "SolidVector.h"
 
-SolidVector::SolidVector(double x, double y, double z, double rotZ /*=0*/, double rotPhi /*=0*/) :
-	TVector3(x,y,z), rotZ(rotZ), rotPhi(rotPhi)
+SolidVector::SolidVector(double x, double y, double z, double rotZ /*=0*/, double rot_Phi /*=0*/) :
+	TVector3(x,y,z), rotZ(rotZ), rotPhi(rot_Phi)
 {
 
 }
