7e7051f7f6fa5c93eeb9d2ab863e1bfa5b49a147
Karl Smith <ksmit218@utk.edu>
2015-08-14 15:30:20 -0500

    Updated so current and the run log are written at the same time.

--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -21,10 +21,8 @@ fi
 if [ ! -e rootfiles ]; then
 	mkdir rootfiles
 fi
-time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat > log/GEBSort_current.log
+time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat | tee log/GEBSort_current.log > log/GEBSort_run%RUN.log
 echo "GEBSort DONE at `date`"
-
-cp -f log/GEBSort_current.log log/GEBSort_run$RUN.log
 
 tail -n 5 log/GEBSort_run$RUN.log
 
