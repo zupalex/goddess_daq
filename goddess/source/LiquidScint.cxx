e6de582360b23dec0b5a4d72ca60bbd62ba0015b
David Walter <dwalter16@gmail.com>
2015-08-14 12:06:22 -0500

    Starting to work on LiquidScint class

--- goddess/source/LiquidScint.cxx
+++ goddess/source/LiquidScint.cxx
@@ -9,16 +9,48 @@ LiquidScint::LiquidScint(std::string desc) {
 
 }
 
-
 LiquidScint::~LiquidScint() {
 
 }
 
+<<<<<<< Updated upstream
 void LiquidScint::SetRawValue(unsigned int detectorChannel, bool secondaryType, unsigned int rawValue) {
+=======
+void LiquindScint::SetDetID(){
+  if (smallType) posID.append("2x2");
+  else posID.append("4x6");
+
+}
+
+void LiquidScint::SetRawValue(unsigned int detectorChannel, bool smallType, int rawValue) {
+  
+  float *enRaw, *psdRaw, *tacRaw;
+  if(smallType){
+    enRaw
+  }
+
+
+
+
+  if(detectorChannel < numChannels)
+    SetRawValue(detectorChannel,true,rawValue);
+  else if(detectorChannel > numChannels + numChannels)
+    SetRawValue(detectorChannel,false,rawValue);
+  else 
+    std::cerr << "ERROR: cannot set raw value for individual channel: " << detectorChannel << "!\n"; 
+
+  if(secondaryType) {
+    largeType += GetEnergy(detectorChannel,rawValue); 
+  }
+  else {
+    smallType += GetEnergy(detectorChannel,rawValue);
+  }
+>>>>>>> Stashed changes
 
 }
 
 void LiquidScint::Clear() {
+
 
 }
 
