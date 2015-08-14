37ba46fb68c74c9db0207fa220e686c50a9b9020
Karl Smith <ksmit218@utk.edu>
2015-08-14 18:13:02 -0500

    Corrected typo in gebsort.sh

--- scripts/gebsort.sh
+++ scripts/gebsort.sh
@@ -21,7 +21,7 @@ fi
 if [ ! -e rootfiles ]; then
 	mkdir rootfiles
 fi
-time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat | tee log/GEBSort_current.log > log/GEBSort_run%RUN.log
+time ./GEBSort_nogeb -input disk $DIR/GEBMerged_run$RUN.gtd_000 -rootfile rootfiles/run$RUN.root RECREATE -chat chatfiles/GEBSort.chat | tee log/GEBSort_current.log > log/GEBSort_run$RUN.log
 echo "GEBSort DONE at `date`"
 
 tail -n 5 log/GEBSort_run$RUN.log
