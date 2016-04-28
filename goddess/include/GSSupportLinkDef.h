be003f51fdccc3c5b6e72d73153991e1e2d4a425
zupalex <alex.lepailleur@gmail.com>
2016-04-28 13:38:33 -0400

    Delete GSSupportLinkDef.h
    file not needed anymore since replaced by another one

--- goddess/include/GSSupportLinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class PNPair<float>+;
-#pragma link C++ class PNPair<int>+;
-#pragma link C++ class SiDetEnStripInfo+;
-
-#pragma link C++ class vector<SiDetEnStripInfo>+;
-#endif
