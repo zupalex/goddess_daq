28d8b9e6b5f990d86cce6b26712cd8935cbb009c
Alex <alex.lepailleur@gmail.com>
2016-05-25 14:57:45 -0400

    Attempt to fix the loading of the analysis macros

--- scripts/goddess_analysis_macro.cxx
+++ scripts/goddess_analysis_macro.cxx
@@ -4,28 +4,28 @@
 #include "TROOT.h"
 #include "TSystem.h"
 
-void LinuxLibrariesLoader()
+void LinuxLibrariesLoader(string myPath)
 {
-  gSystem->Load("./libORRUBA.so");
-  gSystem->Load("./libGoddessStruct.so");
+  gSystem->Load(Form("%s/exec/libORRUBA.so", myPath.c_str()));
+  gSystem->Load(Form("%s/exec/libGoddessStruct.so", myPath.c_str()));
 }
 
-void MacLibrariesLoader()
+void MacLibrariesLoader(string myPath)
 {
-  gSystem->Load("./libORRUBA.dylib");
-  gSystem->Load("./libGoddessStruct.dylib");
+  gSystem->Load(Form("%s/exec/libORRUBA.dylib", myPath.c_str()));
+  gSystem->Load(Form("%s/exec/libGoddessStruct.dylib", myPath.c_str()));
 }
 
-void goddess_analysis_macro()
+void goddess_analysis_macro(string myPathToGoddessDaq)
 {
   #if __APPLE__
-  MacLibrariesLoader();  
+  MacLibrariesLoader(myPathToGoddessDaq);  
   #elif __linux__
-  LinuxLibrariesLoader();
+  LinuxLibrariesLoader(myPathToGoddessDaq);
   #elif __unix__ 
-  LinuxLibrariesLoader();
+  LinuxLibrariesLoader(myPathToGoddessDaq);
   #endif
   
-  gROOT->ProcessLine(".L ./calibstrips.cxx++"); 
-  gROOT->ProcessLine(".L ./MakeEventLists.cxx++");
+  gROOT->ProcessLine(Form(".L %s/exec/calibstrips.cxx++", myPathToGoddessDaq.c_str())); 
+  gROOT->ProcessLine(Form(".L %s/exec/MakeEventLists.cxx++", myPathToGoddessDaq.c_str()));
 }
