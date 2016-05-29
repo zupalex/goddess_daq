844db63bc7ac388c014d3f961b0cf6454c608de3
Alex <alex.lepailleur@gmail.com>
2016-05-29 17:06:02 -0400

    Added a script to make resistive strips calibration easier
    The script has a function to edit the config file directly and replace the old calibration parameters with the new ones.

---
+++ scripts/CalibResistiveStrips.cxx
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "TObject.h"
+#include "TCanvas.h"
+#include "TList.h"
+#include "TLine.h"
+
+std::map<string, double[4][5]> resistiveStripsCalMap;
+
+void GetCornersCoordinates ( TCanvas* can, bool isUpstream, unsigned short sector, unsigned short strip, string detectorType = "SuperX3" )
+{
+    TList* list = can->GetListOfPrimitives();
+
+    int listSize = list->GetSize();
+
+    TLine* negLine = 0;
+    TLine* topLine = 0;
+    TLine* botLine = 0;
+
+    double tempSlope = 0;
+    TLine* tempLine = 0;
+
+    for ( int i = 0; i < listSize; i++ )
+    {
+        TObject* child = list->At ( i );
+
+        TLine* line = dynamic_cast<TLine*> ( child );
+
+        if ( line == NULL ) continue;
+
+        double x1 = line->GetX1();
+        double y1 = line->GetY1();
+        double x2 = line->GetX2();
+        double y2 = line->GetY2();
+
+        double slope = ( y2 - y1 ) / ( x2 - x1 );
+
+//         std::cout << "Found a line in the list at position #" << i << std::endl;
+//         std::cout << "X1 : " << x1 << "   /   Y1 : " << y1 << std::endl;
+//         std::cout << "X2 : " << x2 << "   /   Y2 ; " << y2 << std::endl;
+//
+//         std::cout << "Slope = " << slope << std::endl;
+
+        if ( slope < 0 ) negLine = line;
+        else
+        {
+            if ( tempLine == NULL )
+            {
+                tempSlope = slope;
+                tempLine = line;
+            }
+            else
+            {
+                if ( slope > tempSlope )
+                {
+                    topLine = line;
+                    botLine = tempLine;
+                }
+                else
+                {
+                    botLine = line;
+                    topLine = tempLine;
+                }
+            }
+        }
+    }
+
+    // ---------------- Let's find the inteserction between top and bottom ---------------- //
+
+    double slopeTop, slopeBot, slopeNeg;
+    double offTop, offBot, offNeg;
+
+    slopeTop = ( topLine->GetY2() - topLine->GetY1() ) / ( topLine->GetX2() - topLine->GetX1() );
+    slopeBot = ( botLine->GetY2() - botLine->GetY1() ) / ( botLine->GetX2() - botLine->GetX1() );
+    slopeNeg = ( negLine->GetY2() - negLine->GetY1() ) / ( negLine->GetX2() - negLine->GetX1() );
+
+    offTop = ( topLine->GetX1() * topLine->GetY2() - topLine->GetX2() * topLine->GetY1() ) / ( topLine->GetX1() - topLine->GetX2() );
+    offBot = ( botLine->GetX1() * botLine->GetY2() - botLine->GetX2() * botLine->GetY1() ) / ( botLine->GetX1() - botLine->GetX2() );
+    offNeg = ( negLine->GetX1() * negLine->GetY2() - negLine->GetX2() * negLine->GetY1() ) / ( negLine->GetX1() - negLine->GetX2() );
+
+//     std::cout << "Neg Line : " << slopeNeg << " * x + " << offNeg << std::endl;
+//     std::cout << "Top Line : " << slopeTop << " * x + " << offTop << std::endl;
+//     std::cout << "Bottom Line : " << slopeBot << " * x + " << offBot << std::endl;
+
+    double xIntersect, yIntersect;
+
+    xIntersect = ( offTop - offBot ) / ( slopeBot - slopeTop );
+    yIntersect = slopeTop * xIntersect + offTop;
+
+    std::cout << "Intersection between top and bottom at ( " << xIntersect << " ; " << yIntersect << " )" << std::endl;
+    std::cout << "Slope of the \"Negative\" line = " << slopeNeg << std::endl;
+
+    string calMapKey = detectorType;
+    calMapKey += isUpstream ? " U" : " D";
+    calMapKey += std::to_string ( sector );
+
+    auto calRes = resistiveStripsCalMap[calMapKey][strip];
+
+    calRes[0] = 1;
+    calRes[1] = xIntersect;
+    calRes[2] = yIntersect;
+    calRes[3] = slopeNeg;
+    calRes[4] = offNeg - yIntersect;
+
+    return;
+}
+
+bool DumpFileToResCalMap ( string fileName )
+{
+    std::ifstream readFile ( fileName.c_str() );
+
+    if ( !readFile.is_open() )
+    {
+        std::cerr << "Failed to open file " << fileName << std::endl;
+        return false;
+    }
+
+    string dump;
+
+    string lineID, detID, stripID, dummy;
+
+    std::cout << "Reading " << fileName << "..." << std::endl;
+
+    while ( std::getline ( readFile, dump ) )
+    {
+        istringstream readLine;
+        readLine.str ( dump );
+
+        readLine >> lineID;
+
+        if ( lineID == "SuperX3" )
+        {
+            readLine >> detID;
+
+            detID = lineID + " " + detID;
+
+            int posInFile = readFile.tellg();
+
+            std::cout << "Found " << detID << " at position " << posInFile << " in input file..." << std::endl;
+
+            std::getline ( readFile, dump );
+
+            readLine.clear();
+            readLine.str ( dump );
+
+            readLine >> lineID;
+        }
+
+
+        if ( lineID == "Res." )
+        {
+            readLine >> dummy >> stripID;
+
+            unsigned short stripNbr = std::stoul ( stripID.substr ( 1 ) );
+
+            float xinter, yinter, slope_gm, slope_encal;
+
+            readLine >> xinter >> yinter >> slope_gm >> slope_encal;
+
+            auto readCal = resistiveStripsCalMap[detID][stripNbr];
+
+            readCal[0] = 1;
+            readCal[1] = xinter;
+            readCal[2] = yinter;
+            readCal[3] = slope_gm;
+            readCal[4] = slope_encal;
+        }
+    }
+
+    return true;
+}
+
+void WriteResCalResults ( string fileName, string mode = "recreate" )
+{
+    if ( mode == "update" )
+    {
+        if ( DumpFileToResCalMap ( fileName.c_str() ) ) remove ( fileName.c_str() );
+    }
+
+    std::ofstream outStream ( fileName.c_str() );
+
+    outStream << "Columns are: \"Strip #\"      \"Offset X\"      \"Offset Y\"      \"Slope (gain match)\"     \"Slope (energy calibration)\"\n";
+
+    for ( auto itr = resistiveStripsCalMap.begin(); itr != resistiveStripsCalMap.end(); itr++ )
+    {
+        outStream << "\n" << itr->first << "\n";
+
+        for ( unsigned short i = 0; i < 4; i++ )
+        {
+            if ( itr->second[i][0] == 1 )
+            {
+                outStream << "Res. Strip #" << i << "   " << itr->second[i][1] << "   " << itr->second[i][2] << "   " << itr->second[i][3] << "   " << itr->second[i][4] << "\n";
+            }
+        }
+    }
+
+    return;
+}
+
+bool UpdateResParamsInConf ( string configFile, bool invertContactMidDet = true, string mode = "protected" )
+{
+    std::ifstream readFile ( configFile.c_str() );
+
+    string path = configFile.substr ( 0, configFile.find_last_of ( "/" ) );
+
+    int foundExt = configFile.find ( "." );
+
+    string newName = configFile;
+    newName.insert ( foundExt, "_new" );
+
+    std::ofstream outStream ( newName.c_str() );
+
+    if ( !readFile.is_open() )
+    {
+        std::cerr << "Failed to open file " << configFile << std::endl;
+        return false;
+    }
+
+    string dump;
+
+    string lineID, detID, stripID, dummy;
+
+    std::cout << "Reading " << configFile << "..." << std::endl;
+
+    while ( std::getline ( readFile, dump ) )
+    {
+        istringstream readLine;
+        readLine.str ( dump );
+
+        readLine >> lineID;
+
+        if ( lineID != "superX3" )
+        {
+            outStream << dump << "\n";
+            continue;
+        }
+
+        else
+        {
+            outStream << dump << "\n";
+
+            readLine >> dummy >> detID;
+
+            int posInFile = readFile.tellg();
+
+            std::cout << "Found a superX3 entry: " << detID << std::endl;
+
+            std::getline ( readFile, dump );
+            outStream << dump << "\n";
+
+            std::getline ( readFile, dump );
+            outStream << dump << "\n";
+
+            string detKey = "SuperX3 " + detID.substr ( 0,2 );
+
+            if ( resistiveStripsCalMap.find ( detKey ) != resistiveStripsCalMap.end() )
+            {
+                for ( int i =0; i < 4; i++ )
+                {
+                    if ( resistiveStripsCalMap[detKey][i][0] == 1 )
+                    {
+                        std::cout << "Replacing the parameters for strip #" << i << " with the new values..." << std::endl;
+
+                        std::getline ( readFile, dump );
+
+                        readLine.clear();
+                        readLine.str ( dump );
+
+                        if ( i < 2 || !invertContactMidDet )
+                        {
+                            outStream << "enCal p " << i*2 << " " << resistiveStripsCalMap[detKey][i][2] << " " << 1 << "\n";
+                            outStream << "enCal p " << i*2 + 1 << " " << resistiveStripsCalMap[detKey][i][1] << " " << resistiveStripsCalMap[detKey][i][3] << "\n";
+                            outStream << "enCal resStrip " << i << " 0 " << resistiveStripsCalMap[detKey][i][4] << "\n";
+                        }
+                        else
+                        {
+                            outStream << "enCal p " << i*2 + 1 << " " << resistiveStripsCalMap[detKey][i][1] << " " << resistiveStripsCalMap[detKey][i][3] << "\n";
+                            outStream << "enCal p " << i*2 << " " << resistiveStripsCalMap[detKey][i][2] << " " << 1 << "\n";
+                            outStream << "enCal resStrip " << i << " 0 " << resistiveStripsCalMap[detKey][i][4] << "\n";
+                        }
+                    }
+                    else
+                    {
+                        for ( int i =0; i < 3; i++ )
+                        {
+                            std::getline ( readFile, dump );
+                            outStream << dump << "\n";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for ( int i = 0; i < 12; i++ )
+                {
+                    std::getline ( readFile, dump );
+                    outStream << dump << "\n";
+                }
+            }
+
+
+            for ( int i = 0; i < 4; i++ )
+            {
+                std::getline ( readFile, dump );
+                outStream << dump << "\n";
+            }
+        }
+    }
+
+    readFile.close();
+    outStream.close();
+
+    if ( mode == "overwrite" )
+    {
+        remove ( configFile.c_str() );
+        rename ( newName.c_str(), configFile.c_str() );
+    }
+
+    return true;
+}
+
+
